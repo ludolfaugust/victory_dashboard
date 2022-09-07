53dd3fa55a62f337a042999cfb8f9aa8fd859ad2
Ludolf Von katte <ludolfvonkatte@Ludolfs-Air.fritz.box>
2022-09-07 13:16:22 +0200

    add navbar

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./navbar.scss";
+import PersonIcon from '@mui/icons-material/Person';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const Navbar = () => {
+
+
+    const handleOnClick = () => {
+    
+    alert("work in progress")
+    
+    }
+
+
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <div className="input_wrapper">
+                    <input placeholder="Suchen..."/>
+                </div>                
+                <div className="items">                    
+                        <div className="item">                          
+                            <PersonIcon className="icon" onClick={handleOnClick}/>
+                            <MenuIcon className="icon" />                                                     
+                        </div>                                 
+                </div>
+            </div>
+
+        </div>
+            
+        
+    );
+};
+
